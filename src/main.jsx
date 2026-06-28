284af6250a2a8e85778fe050b59768d896e0f279
Sumit16git <147413767+Sumit16git@users.noreply.github.com>
2026-06-28 12:46:20 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
@@ -9,10 +9,10 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <HashRouter>
+      <BrowserRouter>
         <Toaster position="top-center" reverseOrder={false} />
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </GoogleOAuthProvider>
   </StrictMode>,
 );
